189aeebf9b01886ead5e2daf5dc658bccf1b46bf
Tusharvasshistha <38781446+Tusharvasshistha@users.noreply.github.com>
2024-04-01 11:17:18 +0530

    assignment completed

---
+++ Answer_1.ps1
@@ -0,0 +1,70 @@
+﻿function Convert-Words {
+    param (
+        [int64]$Number
+    )
+
+    # Define arrays for the word representations
+    $units = @("", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine")
+    $teens = @("Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen")
+    $tens = @("", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety")
+    $thousands = @("", "Thousand", "Million", "Billion", "Trillion", "Quadrillion", "Quintillion")
+
+    # Define a function to convert a number less than 1000 to words
+    function Convert-Thousand {
+        param (
+            [int64]$num
+        )
+       
+        if ($num -eq 0) {
+            return ""
+        }
+        elseif ($num -lt 10) {
+            return $units[$num]
+        }
+        elseif ($num -lt 20) {
+            return $teens[$num - 10]
+        }
+        elseif ($num -lt 100) {
+            return "$($tens[$num / 10]) $($units[$num % 10])"
+        }
+        else {
+            return "$($units[$num / 100]) Hundred $(Convert-Thousand ($num % 100))"
+        }
+    }
+
+    if ($Number -eq 0) {
+        return "Zero"
+    }
+
+    $words = ""
+    for ($i = 0; $i -lt $thousands.Length; $i++) {
+        if ($Number % 1000 -ne 0) {
+            $words = "$(Convert-Thousand ($Number % 1000)) $($thousands[$i]) $($words)"
+        }
+        $Number = [math]::DivRem($Number, 1000, [ref]$null)
+    }
+
+    return $words.Trim()
+}
+
+try{
+$Outut = while ($true) {
+    $userInput = Read-Host "Enter a number"
+    if ($userInput -match '^\d+$') {
+        $userInput = [int64]$userInput
+        if ($userInput -lt 0 -or $userInput -gt [int64]::MaxValue) {
+            Write-Host "Please enter a valid integer within the range of 0 to $([int64]::MaxValue)"
+            continue
+        }
+        Write-Host "Program Output: $(Convert-Words $userInput)"
+        break
+    }
+    else {
+        Write-Host "Please enter a valid integer."
+    }
+}
+
+}
+Catch{
+ Write-Output "Error while covert number to words: $_. "
+}
---
+++ Answer_7.ps1
@@ -0,0 +1,47 @@
+﻿function Update-VMState{
+ param(
+        [string]$SubscriptionID,
+
+        [string]$VMName,
+
+        [string]$VMResourceGroupName
+
+    )
+
+
+
+begin {
+    Write-Output "VM RG received is $VMResourceGroupName"
+    Write-Output "VM Name received is $VMName"
+}
+
+process
+{
+
+try{
+##az login  - will be executed via command line or spn can also be used - here mentioning just to make flow more logical
+#az login
+
+##select subscription
+az account set --subscription $SubscriptionID
+
+##Restart the VM
+az vm restart --resource-group $VMResourceGroupName --name $VMName
+
+}
+Catch{
+ Write-Output "Error while restaring the VM: $VMName, $_. "
+}
+
+}
+
+end {
+Write-Output "VM has been started"
+#Addiontionally we can check state of VM just to make a validation
+}
+
+}
+
+
+##RUn function
+Update-VMState -SubscriptionID "abcd-efgh-ijkl" -VMName "myvm" -VMResourceGroupName "myrg"
